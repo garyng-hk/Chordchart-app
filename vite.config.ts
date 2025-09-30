0b137508b05bdb9822679f3a0b2de26b9d614650
garyng-hk <gary.ng2@gmail.com>
2025-09-30 17:50:07 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       // 在這裡加入 base 屬性！
-      base: '/ylgc-chordchart-search/', 
+      base: '/Chordchart-app/', 
 
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
